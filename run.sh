36a135d96ab8d97d93b6a3c9bd4808a9c75a284f
Vincent Demeester <vincent@sbr.pm>
2015-06-14 15:14:51 +0200

    Add some more stuff around (static server, demo notes, ..)

--- run.sh
+++ run.sh
@@ -1,10 +1,24 @@
 #!/bin/bash
+
+echo "Clean previous container"
+docker kill tz-static tz-prez
+docker rm -f tz-static tz-prez || true
+
+echo "Build zenika/go-server image.."
+docker build -t zenika/go-server -f static/server/Dockerfile
+echo "Run it on local port 9000"
+docker run -d -p 80:8000 \
+       -v $PWD/static:/go/src/app/static \
+       --name tz-static \
+       zenika/go-server
+
 echo "Build tz-docker-night-light image.."
 docker build -t tz-docker-night-light .
 echo "Run it on local port 8000"
-docker run --rm -it -p 8000:8000 \
+docker run -d -p 8000:8000 \
        -v $PWD/CahierExercices:/data/CahierExercices \
        -v $PWD/Exercices:/data/Exercices \
        -v $PWD/Slides:/data/Slides \
        -v $PWD/Gruntfile.js:/data/Gruntfile.js \
+       --name tz-prez \
        tz-docker-night-light
